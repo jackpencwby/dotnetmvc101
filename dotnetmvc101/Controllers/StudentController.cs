574446ac1464f4161dc937c4541a687602da72ef
jackpencwby <kajonyod.jack@gmail.com>
2024-12-01 12:52:34 +0700

    สร้าง obj หลายๆ obj ของ class Student ในไฟล์ StudentController.cs, ดึงข้อมูลหลายๆ obj มาใช้ในไฟล์ ShowAllStudentScore.cshtml

--- dotnetmvc101/Controllers/StudentController.cs
+++ dotnetmvc101/Controllers/StudentController.cs
@@ -17,10 +17,25 @@ namespace dotnetmvc101.Controllers
 
         public IActionResult ShowAllStudentScore()
         {
-            Student s = new Student();
-            s.StudentID = "66010089";
-            s.Name = "Jack";
-            s.Score = 99;
+            Student s1 = new Student();
+            s1.StudentID = "66010089";
+            s1.Name = "Jack";
+            s1.Score = 99;
+
+            Student s2 = new Student();
+            s2.StudentID = "66000001";
+            s2.Name = "Alice";
+            s2.Score = 75;
+
+            Student s3 = new Student();
+            s3.StudentID = "66000002";
+            s3.Name = "Bob";
+            s3.Score = 49;
+
+            List<Student> s = new List<Student>();
+            s.Add(s1);
+            s.Add(s2);
+            s.Add(s3);
 
             return View(s);
         }
