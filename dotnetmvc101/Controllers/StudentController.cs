ea175ddf95ded445edd8519913b7977cfc1a3dee
jackpencwby <kajonyod.jack@gmail.com>
2024-12-01 11:49:37 +0700

    สร้างไฟล์ StudentController.cs เเละเขียนโค้ด เเละเเก้ไขไฟล์ Program.cs ให้ route เริ่มต้น map กับ StudentController

---
+++ dotnetmvc101/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetmvc101.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("หน้าเเรก");
+        }
+
+        public IActionResult SaveScore()
+        {
+            return Content("บันทึกคะเเนนสอบวิชา Web Application");
+        }
+
+        public IActionResult ShowAllStudentScore()
+        {
+            return Content("คะเเนนสอบวิชา Web Application");
+        }
+
+        public IActionResult ShowStudentScore(string id)
+        {
+            return Content($"คะเเนนสอบวิชา Web Application ของรหัสนักศึกษา : {id}");
+        }
+    }
+}
