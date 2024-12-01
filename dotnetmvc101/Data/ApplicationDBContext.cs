caa89349066f82135d1517c9411d94024103a162
jackpencwby <kajonyod.jack@gmail.com>
2024-12-01 17:33:50 +0700

    ติดตั้ง Entity Framework Core เเละสร้างโฟลเดอร์ Data เเละสร้างไฟล์ ApplicationDBContext.cs พร้อมกับเขียนโค้ด

---
+++ dotnetmvc101/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using dotnetmvc101.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetmvc101.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
