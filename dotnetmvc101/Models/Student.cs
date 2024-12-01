7fe3dcd3d3833c1a396f2487f7ae5c417a500f9a
jackpencwby <kajonyod.jack@gmail.com>
2024-12-01 12:22:16 +0700

    สร้างไฟล์ Student.cs ใน Models เเละเขียนโค้ด

---
+++ dotnetmvc101/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace dotnetmvc101.Models
+{
+    public class Student
+    {
+        private string id;
+        private string name;
+        private int score;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+    }
+}
